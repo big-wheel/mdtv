463309be63360d8390e3d59d11bed781d5a28e49
Claiyre_ <mayingying1996@163.com>
2018-01-22 23:45:57 +0800

    nothing

--- bin/mdtv.js
+++ bin/mdtv.js
@@ -1,3 +1,13 @@
-var chalk = require('chalk');
-var path = require('path');
+#!/usr/bin/env node 
+
+// var chalk = require('chalk');
+// var path = require('path');
 var commander = require('commander');
+var packageInfo = require('../package.json');
+
+commander
+  .version(packageInfo.version);
+
+commander.command('');
+
+
