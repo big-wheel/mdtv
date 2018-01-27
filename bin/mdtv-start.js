9cb009e39c21fb1de812f1b6c465c68604ec4285
Claiyre <ma_yingying2@163.com>
2018-01-27 23:20:31 +0800

    md -> html

--- bin/mdtv-start.js
+++ bin/mdtv-start.js
@@ -5,6 +5,9 @@ var fs = require('fs');
 var log = console.log;
 var app = express();
 var route = require('./routes');
+var remark = require('remark');
+// var styleGuide = require('remark-preset-lint-markdown-style-guide');
+var html = require('remark-html');
 
 app.set('views', path.resolve('public/template'));
 app.set('view engine', 'jsx');
@@ -21,9 +24,17 @@ function readFile(req, res, next) {
   log(fs.existsSync(abp));
   if(fs.existsSync(abp) && fs.lstatSync(abp).isFile()) {
     fs.readFile(abp, (err, data) => {
-      var d = '<pre>' + data + '</pre>';
-      log(d);
-      res.send(d);
+      if(req.path.slice(req.path.length - 3) === '.md'){
+        remark()
+          .use(html)
+          .process(data, function (err, file) {
+            log(chalk.green(file));
+            res.send(file.contents);
+          });
+      } else {
+        var d = '<pre>' + data + '</pre>';
+        res.send(d);
+      }
     });
   } else {
     next();
