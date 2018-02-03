e42d74e06a3ab3901ade9ea177d940eabe33df28
Claiyre <ma_yingying2@163.com>
2018-02-03 17:16:57 +0800

    add options to config if show toc and improve CLI options

--- bin/mdtv.js
+++ bin/mdtv.js
@@ -11,15 +11,17 @@ var log = console.log;
 
 commander
   .version(pck.version)
-  .option('-v, --version')
-  .option('-m, --mdName <mdName>')
+  .option('-m, --mdName <mdName>', 'specify the name of md file to show')
   .option('-p, --port <port>', 'set the port')
+  .option('-t, --toc', 'whether to show table of content of md files')
   .parse(process.argv);
 
 log(chalk.yellow(`mdtv ${pck.version} running`));
 
 let port = commander.port || 8089;
 let mdName = commander.mdName;
+
+global.toc = !!commander.toc;
 
 require('./mdtv-start')(mdName, port);
 
